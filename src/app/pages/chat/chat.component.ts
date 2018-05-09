f91ced2d90f261040e05ad8b13b59d7d17a892c4
Rodrigo Mendoza <ruymenca1@gmail.com>
2018-05-09 10:45:06 -0500

    Avance hasta el 09

--- src/app/pages/chat/chat.component.ts
+++ src/app/pages/chat/chat.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { ChatService } from '../../services/service.index';
+import { ChatService, UsuarioService } from '../../services/service.index';
+import { URL_SERVICIOS } from '../../config/config';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-chat',
@@ -8,12 +11,41 @@ import { ChatService } from '../../services/service.index';
 })
 export class ChatComponent implements OnInit {
 
-  constructor( public _chatService: ChatService ) {
-    
+  contactos = new Array;
+
+  constructor( 
+    public _chatService: ChatService, public _usuarioService: UsuarioService,
+    public http:HttpClient
+   ) {
+     this.cargarContactos();
   }
 
   ngOnInit() {
   }
+  
+  cargarContactos(){
+    this._usuarioService.cargarUsuarios().subscribe( resp => {
+      console.log(resp);
+      this.contactos = resp['usuarios'];
+    });
+  }
 
+  buscarContacto( nombre:string ){
+    if( nombre.length === 0 ){
+      this.cargarContactos();
+    }
+    console.log(nombre);
+    let url = URL_SERVICIOS + '/busqueda/coleccion/usuarios/' + nombre;
+
+    this.http.get( url )
+        .subscribe( (resp: any) => {
+          this.contactos = resp.usuarios;
+          console.log(resp);
+        });
+  }
+
+  cargarConversation( idReservation:string ){
+  console.log(idReservation);
+  }
 
 }
--- src/app/services/chat/chat.service.ts
+++ src/app/services/chat/chat.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from '@angular/core';
 import * as io from 'socket.io-client'
+import { HttpClient } from '@angular/common/http';
+import { URL_SERVICIOS } from '../../config/config';
 
 @Injectable()
 export class ChatService {
 
   public socket = io('http://localhost:3000');
 
-  constructor() {
+  constructor(
+  ) {
     
   };
+
 
 }
